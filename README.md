43998aa77530725bdcdad6bdcba12cb46cce8f41
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2016-05-31 18:37:18 +0530

    Setting up the Project
    Started with a Spring Starter Project, no dependencies selected
    Deleted classes from src/test/java
    Removed the spring-boot-starter-test dependency from the pom.xml

---
+++ README.md
@@ -0,0 +1,3 @@
+* Started with a Spring Starter Project, no dependencies selected
+* Deleted classes from src/test/java
+* Removed the spring-boot-starter-test dependency from the pom.xml
---
+++ src/main/java/in/mhatre/sagar/spring/tutorial/point/SpringTutorialsPointApplication.java
@@ -0,0 +1,12 @@
+package in.mhatre.sagar.spring.tutorial.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialsPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialsPointApplication.class, args);
+	}
+}
